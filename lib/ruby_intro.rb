fa567cf0c5fe87d0215a5c0e48e08f129a49f69f
Eric B <ericbechtold4@hotmail.com>
2016-09-02 22:23:29 +0000

    Part 1 now passes tests perfectly

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,41 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sumOfArr = 0;
+  if arr.length > 0
+    sumOfArr = arr.inject{|accum, curr| accum + curr};
+  end
+  return sumOfArr
+  
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  elsif arr.length == 1
+    return arr[0]
+  else
+    temp_arr = arr.sort()
+    highNum1 = temp_arr[temp_arr.length - 1]
+    highNum2 = temp_arr[temp_arr.length - 2]
+    return highNum1 + highNum2
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.length == 0 || arr.length == 1
+    return false
+  else
+    arr.each_index do |currIndex|
+      numNeeded = n-arr[currIndex]
+      indexOfNum2 = arr.index{|x| x == numNeeded}
+      puts indexOfNum2
+      if indexOfNum2 && indexOfNum2 != currIndex
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
